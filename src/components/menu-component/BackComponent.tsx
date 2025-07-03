610413c71f92299950dfa1d231a5753c0f53b27d
aleks1303 <aleksbulda13@gmail.com>
2025-07-03 16:03:18 +0300

    init

--- src/components/menu-component/BackComponent.tsx
+++ src/components/menu-component/BackComponent.tsx
@@ -5,13 +5,15 @@ import { useRouter } from 'next/navigation';
 
 const BackComponent = () => {
     const router = useRouter();
-
+    const handler = () => {
+        router.back()
+    }
     return (
         <div>
             <ul>
                 <li className="flex justify-center mt-10">
                     <button
-                        onClick={() => router.back()}
+                        onClick={handler}
                         className="border border-gray-500 rounded px-4 py-2"
                     >
                         Назад
