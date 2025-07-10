3f613194ce5dcc0cb946287cb005fdee42911f5f
aleks1303 <aleksbulda13@gmail.com>
2025-07-10 13:15:22 +0300

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <title>Project Movie</title>
+      </head>
       <body
       >
-        {children}
+      {children}
       </body>
-    </html>
+      </html>
   );
 }
